91f65cd3c9e3f6bac72e5d1798569d03061a52a9
strukturwandel-heide-dresden <hirschfranzi@web.de>
2026-03-02 12:56:56 +0100

    Update webmap 02.03.2026

---
+++ webmap/qgis2web_abschlussprojekt/styles/Dresden_4_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Dresden_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#959595";
+    var bufferWidth = 0.6000000000000001;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("name") !== null && resolution > 19 && resolution < 700) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(225,89,137,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
